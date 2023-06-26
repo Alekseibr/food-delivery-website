0b70f6f31a9634fe5efa31eec1c0d7312bba3dfc
Alexey Breev <bank.86@mail.ru>
2023-06-26 23:08:38 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get('/pizza', (req, res) => {
+app.get('/backend/pizza', (req, res) => {
   fs.readFile(__dirname +'/db.json', 'utf8', function (err, data) {
       if (err) throw err;
        obj = JSON.parse(data);
